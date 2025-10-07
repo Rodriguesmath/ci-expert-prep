6244852e1ce930d1bb99535819593b6fb24b6582
Rodrigues Lima <rodrigues-lima.rl@academico.ifpb.edu.br>
2025-10-07 14:54:24 -0300

    update: adicionando link ao footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-gray-900 text-center p-6 text-gray-500 mt-20 border-t border-gray-800">
             <p>Criado para auxiliar nos estudos do programa CI Expert.</p>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="inline-flex items-center hover:text-cyan-400 transition mt-2">
+            <a href="https://github.com/Rodriguesmath/ci-expert-prep" target="_blank" rel="noopener noreferrer" className="inline-flex items-center hover:text-cyan-400 transition mt-2">
                 <Github size={16} className="mr-2" />
                 Veja o código no GitHub
             </a>
